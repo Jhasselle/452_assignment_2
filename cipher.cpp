ffa925dcf1b22bfd2c5bb14f09d95e8aab1ac758
Jason Hasselle <Jhasselle@gmail.com>
2019-04-12 15:57:30 -0700

    setting up correct block sizes

--- cipher.cpp
+++ cipher.cpp
@@ -1,11 +1,12 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 #include "CipherInterface.h"
 #include "DES.h"
 #include "AES.h"
-
-#define VALID_KEY_LENGTH 16
 using namespace std;
+#define DES_KEY_LENGTH 16
+#define AES_KEY_LENGTH 32
 
 int main(int argc, char** argv)
 {
@@ -18,14 +19,18 @@ int main(int argc, char** argv)
 	 * misbehave.
 	 */
 
+
 	string cipherType = argv[1];
 	char * cipherKeyString = argv[2];
-	unsigned char cipherKey [VALID_KEY_LENGTH];
+	string cipherKeyStringSpiteCpp = argv[2];
 	string encodingType = argv[3];
 	char * inputFileName = argv[4];
 	char * outputFileName = argv[5];
-	
+
 	CipherInterface* cipher = NULL; 
+	unsigned int cipherKeyLength = cipherKeyStringSpiteCpp.length();
+	unsigned char cipherKey [cipherKeyLength];
+	unsigned int blockSize = 0;
 
 	// Check if all arguments are present
 	if (!outputFileName || !inputFileName) {
@@ -34,8 +39,8 @@ int main(int argc, char** argv)
 		exit(-1);
 	}
 
-	// check key length
-	for (int i = 0; i < VALID_KEY_LENGTH; ++i) {
+		// check key length
+	for (int i = 0; i < cipherKeyLength; ++i) {
 
 		if (cipherKeyString[i] == '\0')
 		{
@@ -47,24 +52,32 @@ int main(int argc, char** argv)
 			cipherKey[i] = cipherKeyString[i];
 		}
 	}
+	
 
 	
-	if (cipherType == "DES" || cipherType == "des" ) {
+
+
+
+	if ((cipherType == "DES" || cipherType == "des") && cipherKeyLength == DES_KEY_LENGTH) {
 		/* Create an instance of the DES cipher */	
 		cipher = new DES(); 
+		blockSize = DES_KEY_LENGTH / 2;
 	}
-	else if (cipherType == "AES" || cipherType == "aes") {
+	else if ((cipherType == "AES" || cipherType == "aes") && cipherKeyLength == AES_KEY_LENGTH)  {
 		cipher = new AES(); 
+		blockSize = AES_KEY_LENGTH / 2;
 	}
 	else {
-		fprintf(stderr, "ERROR [%s %s %d]: Invalid encryption type. Please use DES or AES\n",	
+		fprintf(stderr, "ERROR [%s %s %d]: Invalid encryption type. Please use DES or AES \n",	
 		__FILE__, __FUNCTION__, __LINE__);
 		exit(-1);
 	}
 
-	//DES
 
-		
+
+	
+	
+	
 	/* Error checks */
 	if(!cipher)
 	{
@@ -73,11 +86,30 @@ int main(int argc, char** argv)
 		exit(-1);
 	}
 	
-
 	// cipher->setKey((unsigned char*)cipherKey);
 	cipher->setKey(cipherKey);
 	
 	// Open file
+	ifstream inputFileStream;
+	inputFileStream.open (inputFileName);
+	string str = "";
+	int tempInt = 0;
+	while (!inputFileStream.eof()) {
+		unsigned char * block = new unsigned char [blockSize];
+		int i = 0;
+		while (!inputFileStream.eof() || i < blockSize) {
+			++tempInt;
+			unsigned char c = inputFileStream.get();
+			cout << c;
+			++i;
+		}
+		cout << endl;
+		
+	}
+	// inputFileStream >> "Writing this to a file.\n";
+	cout << tempInt;
+	inputFileStream.close();
+
 
 	/* Perform encryption */
 	// string cipherText = cipher->encrypt("hello world");
